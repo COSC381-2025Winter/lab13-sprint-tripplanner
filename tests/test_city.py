7d2cca433641d4f298c24293f2a03961620aba5e
OliverMcMillen <196836907+OliverMcMillen@users.noreply.github.com>
2025-04-20 14:10:43 -0400

    Added 3rd test function:
    - Added test_validCity_entered()
    - Tests data received is of float type.
    - Validates if Ypsilanti, MI is entered, the correct coordinates are returned.
    - All 3 tests in city.py pass successfully.

--- tests/test_city.py
+++ tests/test_city.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"
 from city import getLongAndLat
 
 # Oliver McMillen
-def test_empty_cityst_entered(capsys):
+def test_empty_cityst_entered():
     
     # Arrange
     input_value = ""
@@ -17,12 +17,12 @@ def test_empty_cityst_entered(capsys):
     response = getLongAndLat(input_value)
 
     # Assert
-    captured = capsys.readouterr()
     assert "status" in response
     assert response["status"] in ["ZERO_RESULTS", "INVALID_REQUEST", "ERROR"]
 
+
 #Oliver McMillen
-def test_numerical_cityst_entered(capsys):
+def test_numerical_cityst_entered():
     
     # Arrange
     input_value = "999"
@@ -34,3 +34,23 @@ def test_numerical_cityst_entered(capsys):
     assert "status" in response
     assert response["status"] in ["ZERO_RESULTS", "INVALID_REQUEST", "ERROR"]
 
+
+#Oliver McMillen
+def test_validCity_entered():
+
+    # Arrange
+    input_value = "Ypsilanti, MI"
+
+    # Act
+    response = getLongAndLat(input_value)
+
+    # Assert
+    assert "status" in response
+    assert response["status"] == "OK"
+    location = response["results"][0]["geometry"]["location"]
+
+    assert isinstance(location["lat"], float)
+    assert "42.2411" in str(location["lat"])
+    
+    assert isinstance(location["lng"], float)
+    assert "83.6129" in str(location["lng"])
