834625c47f932f4126a910051ea712269c007675
OliverMcMillen <196836907+OliverMcMillen@users.noreply.github.com>
2025-04-21 19:57:46 -0400

    Updates to test_city.py: - Modified tests for alteration to class files - All 3 tests pass successfully

--- tests/test_city.py
+++ tests/test_city.py
@@ -5,52 +5,47 @@ import sys
 import pytest
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-from city import getLongAndLat
+from city import City
 
 # Oliver McMillen
 def test_empty_cityst_entered():
-    
+
     # Arrange
     input_value = ""
 
     # Act
-    response = getLongAndLat(input_value)
-
+    city = City(input_value)
+    
     # Assert
-    assert "status" in response
-    assert response["status"] in ["ZERO_RESULTS", "INVALID_REQUEST", "ERROR"]
-
+    with pytest.raises(ValueError):
+        city.get_coordinates()
 
 #Oliver McMillen
 def test_numerical_cityst_entered():
-    
+
     # Arrange
     input_value = "999"
 
     # Act
-    response = getLongAndLat(input_value)
+    city = City(input_value)
 
     # Assert
-    assert "status" in response
-    assert response["status"] in ["ZERO_RESULTS", "INVALID_REQUEST", "ERROR"]
-
+    with pytest.raises(ValueError):
+        city.get_coordinates()
 
 #Oliver McMillen
 def test_validCity_entered():
-
     # Arrange
     input_value = "Ypsilanti, MI"
 
     # Act
-    response = getLongAndLat(input_value)
+    city = City(input_value)
+    lat, lng = city.get_coordinates()
+
 
     # Assert
-    assert "status" in response
-    assert response["status"] == "OK"
-    location = response["results"][0]["geometry"]["location"]
+    assert isinstance(lat, float)
+    assert "42.2" in str(lat)
 
-    assert isinstance(location["lat"], float)
-    assert "42.2411" in str(location["lat"])
-    
-    assert isinstance(location["lng"], float)
-    assert "83.6129" in str(location["lng"])
+    assert isinstance(lng, float)
+    assert "-83.6" in str(lng)
