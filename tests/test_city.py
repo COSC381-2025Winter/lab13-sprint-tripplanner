bf95244e98facb093f49633eae265c9475ef2ba2
OliverMcMillen <196836907+OliverMcMillen@users.noreply.github.com>
2025-04-24 15:04:35 -0400

    Revised test files for updated directory structure

--- tests/test_city.py
+++ tests/test_city.py
@@ -4,7 +4,7 @@ import os
 import sys
 import pytest
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/tripplanner")))
 from city import City
 
 # Oliver McMillen
